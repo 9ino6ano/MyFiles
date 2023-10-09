ed0b90073068cb89633238279db40e5b7cf50b53
9ino6ano <124846862+9ino6ano@users.noreply.github.com>
2023-10-09 11:47:18 +0200

    These are Java 5th projects
    These are projects created while studying the BigJava5th

---
+++ ProductPDDA/src/prod/ProductPD.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prod;
+
+/**
+ *
+ * @author Pana
+ */
+public class ProductPD {
+    
+    
+    //DA METHODS
+    public void initialiseConnection() throws DataStorageException{
+        ProductDA.initialiseConnection();
+    }
+    
+}
